c8ebbadcd6148d9684b099260ec0f329b8478ce4
reema sharma <reemasharma4707@gmail.com>
2026-06-09 17:11:09 +0530

    Add programming-elements level-1 folder

---
+++ OneDrive/Desktop/BridgeLabz-SIPP-Training-2026/core-java-practice/gcr-codebase/programming-elements/level1/AveragePCM.java
@@ -0,0 +1,10 @@
+class AveragePCM {
+    public static void main(String[] args) {
+        String name = "Reema";
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+        double averageMarks = (mathsMarks + physicsMarks + chemistryMarks) / 3.0;
+        System.out.println(name + "'s average mark in PCM is " + averageMarks);
+    }
+}
